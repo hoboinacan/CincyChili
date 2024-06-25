5751c9e00c101af0a44a6fba3204cd1382ecaafa
hoboinacan <67512189+hoboinacan@users.noreply.github.com>
2024-06-25 18:41:04 -0400

    First
    Working canned Cincy Chili

---
+++ Contents/mods/CincyChili/media/lua/server/Items/CincyChili_Dist.lua
@@ -0,0 +1,13 @@
+require "Items/ProceduralDistributions"
+
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions["list"]["CrateCannedFood"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions["list"]["GigamartCannedFood"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["GroceryStorageCrate1"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions["list"]["GroceryStorageCrate1"].items, 10);
+
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions["list"]["KitchenCannedFood"].items, 10);
