d00e4f23035be350c856e9b968503a620121b599
hoboinacan <67512189+hoboinacan@users.noreply.github.com>
2024-12-29 15:50:50 -0500

    b42 updates
    Updates to work with b42

---
+++ Contents/mods/CincyChili/42.0/media/lua/server/Items/CincyChili_ProceduralDistributions.lua
@@ -0,0 +1,28 @@
+require "Items/ProceduralDistributions"
+
+table.insert(ProceduralDistributions.list["CrateCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["CrateCannedFood"].items, 6);
+
+table.insert(ProceduralDistributions.list["CrateHumanitarian"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["CrateHumanitarian"].items, 0.01);
+
+table.insert(ProceduralDistributions.list["DerelictHouseSquatter"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["DerelictHouseSquatter"].items, 0.01);
+
+table.insert(ProceduralDistributions.list["GigamartCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["GigamartCannedFood"].items, 6);
+
+table.insert(ProceduralDistributions.list["GroceryStorageCrate1"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["GroceryStorageCrate1"].items, 1);
+
+table.insert(ProceduralDistributions.list["KitchenCannedFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["KitchenCannedFood"].items, 4);
+
+table.insert(ProceduralDistributions.list["SafehouseFood"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["SafehouseFood"].items, 2);
+
+table.insert(ProceduralDistributions.list["SafehouseFood_Mid"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["SafehouseFood_Mid"].items, 4);
+
+table.insert(ProceduralDistributions.list["SafehouseFood_Late"].items, "CincyChili.CannedCincyChili");
+table.insert(ProceduralDistributions.list["SafehouseFood_Late"].items, 1);
